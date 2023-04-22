03c1ea6e02a2ad51ee2eef9dfc9ae24134aa61a8
antoinecln56 <antoine.clain@hotmail.fr>
2023-04-22 18:13:25 +0200

    Utilisation des observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs'
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, OnDestroy {
+  secondes;
+  compteurSubscription: Subscription;
   title = 'labonneaffaire';
 
 
-  constructor(){}
+  constructor() { }
 
-  ngOnInit(){
+  ngOnInit() {
+    const compteur = interval(1000).pipe(
+      filter(value => value % 2 === 0),
+      map(value => value % 2 === 0 ? `${value} est pair` : `${value} est impair`)
+    );
 
+    this.compteurSubscription = compteur.subscribe({
+      next: (v) => this.secondes = v,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.compteurSubscription.unsubscribe();
   }
 
 
